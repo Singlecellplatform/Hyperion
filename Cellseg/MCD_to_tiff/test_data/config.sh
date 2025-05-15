c63893eb79f7a05e06ee5a5de9b416115cd175c7
KuangyingYang <93620719+KuangyingYang@users.noreply.github.com>
2025-05-15 16:36:21 -0500

    Update config.sh

--- Cellseg/MCD_to_tiff/test_data/config.sh
+++ Cellseg/MCD_to_tiff/test_data/config.sh
@@ -4,11 +4,4 @@
 
 # sample list file
 csv_file="/mnt/Sample_list.csv"
- 
-
-# Reference Paths
- 
-Ref_path="/storage1/fs1/${STORAGE_ALLOCATION}/Active/WGS_GEM_reference"
-PY_function_path="/storage1/fs1/hirbea/Active/lyu.yang/PY_function"
-
 
